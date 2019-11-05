d3e4421a561c04dc55a9a836a784d1f51292b07d
Adam Ahrens <adam.aahrens@gmail.com>
2019-11-04 20:35:01 -0600

    Finishing up API for Flask

--- app.py
+++ app.py
@@ -20,9 +20,6 @@ Base.prepare(engine, reflect=True)
 Measurement = Base.classes.measurement
 Station = Base.classes.station
 
-# Create our session (link) from Python to the DB
-session = Session(engine)
-
 #### Setup Flask ####
 app = Flask(__name__) 
 
@@ -32,16 +29,21 @@ def root():
     return """ <h1>Hawaii Climate API</h1>
         <h3>Routes to Use</h3>
         <ul>
-            <li>api/v1.0/precipitation</li>
-            <li>api/v1.0/stations</li>
-            <li>api/v1.0/tobs</li>
-            <li>api/v1.0/{start}</li>
-            <li>api/v1.0/{start}/{end}</li>
+            <li>/api/v1.0/precipitation</li>
+            <li>/api/v1.0/stations</li>
+            <li>/api/v1.0/tobs</li>
+            <li>/api/v1.0/temp/{start}</li>
+            <li>/api/v1.0/temp/{start}/{end}</li>
         </ul>"""
 
 @app.route('/api/v1.0/precipitation')
 def precipitation():
     """Returns precipitation data for the past year"""
+    # Create our session (link) from Python to the DB
+    # Fix issue with accessing SQLite3 on different threads
+    # https://www.reddit.com/r/learnpython/comments/5cwx34/flask_sqlite_error/
+    session = Session(engine)
+
     # Find most recent date in the dataset
     result = session.query(func.max(Measurement.date))[0]
     max_date = result[0]
@@ -54,13 +56,104 @@ def precipitation():
     results = session.query(Measurement.date, Measurement.prcp).filter(
                         Measurement.date >= last_year).filter(
                         Measurement.date <= last_date).all()
+
     # Convert results to dictionary
     precipitation = {}
     for result in results:
         (date, prcp) = result
         precipitation[date] = prcp
 
-    return jsonify(precipitation)
+    # Close the session
+    session.close()
+
+    return jsonify({'precipitation': precipitation})
+
+@app.route('/api/v1.0/stations')
+def stations():
+    """Returns all the station names"""
+    # Create our session (link) from Python to the DB
+    # Fix issue with accessing SQLite3 on different threads
+    # https://www.reddit.com/r/learnpython/comments/5cwx34/flask_sqlite_error/
+    session = Session(engine)
+
+    # Query for the result
+    results = session.query(Station.name,).group_by(Station.name).all()
+    stations = []
+    for result in results:
+        name = result[0]
+        stations.append(name)
+    
+     # Close the session
+    session.close()
+    
+    return jsonify({'stations': stations})
+
+@app.route('/api/v1.0/tobs')
+def tobs():
+    """Returns temperature data for the past year"""
+    # Create our session (link) from Python to the DB
+    # Fix issue with accessing SQLite3 on different threads
+    # https://www.reddit.com/r/learnpython/comments/5cwx34/flask_sqlite_error/
+    session = Session(engine)
+
+    # Find most recent date in the dataset
+    result = session.query(func.max(Measurement.date))[0]
+    max_date = result[0]
+    last_date = dt.datetime.strptime(max_date, '%Y-%m-%d')
+
+    # Calculate one year ago from the most recent date
+    last_year = last_date - dt.timedelta(days=365)
+
+    observations = session.query(
+            Measurement.station, func.count(Measurement.station)).group_by(
+            Measurement.station).order_by(
+            func.count(Measurement.station).desc()).all()
+
+    # Station that has the highest number of observations
+    station_name_highest_observations = observations[0][0]
+
+    # Query for results
+    results = session.query(Measurement.date, Measurement.tobs).filter(
+                        Measurement.date >= last_year).filter(
+                        Measurement.date <= last_date).filter(
+                        Measurement.station == station_name_highest_observations).all()
+
+    # Convert results to dictionary
+    tobs = {}
+    for result in results:
+        (date, tob) = result
+        tobs[date] = tob
+
+    # Close the session
+    session.close()
+
+    return jsonify({station_name_highest_observations : tobs})
+
+@app.route('/api/v1.0/temp/<start>', defaults={'end': None})
+@app.route('/api/v1.0/temp/<start>/<end>')
+def temperatures(start, end):
+    """Return TMIN, TAVG, TMAX from a starting date, or between two dates"""
+    # Create our session (link) from Python to the DB
+    # Fix issue with accessing SQLite3 on different threads
+    # https://www.reddit.com/r/learnpython/comments/5cwx34/flask_sqlite_error/
+    session = Session(engine)
+
+    query = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs))
+    query = query.filter(Measurement.date >= start)
+
+    if end is not None:
+        query = query.filter(Measurement.date <= end)
+    
+    # Execute the query
+    results = query.all()
+
+    # Pretty up return
+    (tmin, tavg, tmax) = results[0]
+
+     # Close the session
+    session.close()
+
+    return jsonify({'temp_min' : tmin, 'temp_avg' : tavg, 'temp_max' : tmax})
 
 #### Run ####
 if __name__ == '__main__':
